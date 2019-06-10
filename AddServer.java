a0efd674306712780fe857c9e3810c842362ca9f
anshuman35 <49508586+anshuman35@users.noreply.github.com>
2019-06-10 12:27:16 +0530

    Update AddServer.java

--- AddServer.java
+++ AddServer.java
@@ -1,11 +1,18 @@
+import java.util.*;
 import java.rmi.*;
 public class AddServer
 {
-public static void main(String arg[])
+
+	
+	public static void main(String arg[])
 {
+		
+		ArrayList z=new ArrayList();
+		
+		
  try
  {
-   AddServiceInterface addService=new Adder();
+   AddServiceInterface addService=new Adder(z);
    Naming.rebind("AddService",addService);
   }
 catch(Exception e)
